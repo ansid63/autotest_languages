b280f00102d950f8ce3517cd9af372de0bf7976e
Andrey <sidorov.avk@gmail.com>
2020-01-22 17:27:49 +0400

    Add right assert

--- test_items.py
+++ test_items.py
@@ -5,6 +5,10 @@ def test_add_button_exist(browser):
     browser.get(link)
 
     time.sleep(5)
+    element_is_displayed = True
 
-    browser.find_element_by_css_selector("button.btn-add-to-basket")
-    assert True, "There is no button here"
+    try:
+        browser.find_element_by_css_selector("button.btn-add-to-basket")
+    except NoSuchElementException:
+        element_is_displayed = False
+    assert element_is_displayed, "There is no button here"
